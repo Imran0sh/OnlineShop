4e6e09a41e0119f3472a06ed8e9d8a364d518a91
imranahmetov-png <imranahmetov28@gmail.com>
2026-08-28 13:23:47 +0500

    feat: cart component

---
+++ src/components/Cart/Cart-page.tsx
@@ -0,0 +1,11 @@
+export default function CartPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-extrabold text-black">Корзина</h1>
+
+      <div className="flex flex-row gap-4 items-center justify-between">
+        <div className="flex flex-wrap gap-2 border"></div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Cart/Cart.tsx
@@ -0,0 +1,9 @@
+import shoppingCart from "@/assets/img/shopping-cart.svg";
+
+export function Cart() {
+  return (
+    <div>
+      <img src={shoppingCart.src} alt="Shopping Cart" />
+    </div>
+  );
+}
